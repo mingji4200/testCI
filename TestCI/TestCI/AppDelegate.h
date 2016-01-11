7dfc813cb345fc00993c259923427865707e970a
sluin <sunlin@sensetime.com>
2016-01-11 17:35:04 +0800

    init .

---
+++ TestCI/TestCI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCI
+//
+//  Created by sluin on 16/1/11.
+//  Copyright © 2016年 SenseTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCI/TestCI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCI
+//
+//  Created by sluin on 16/1/11.
+//  Copyright © 2016年 SenseTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
