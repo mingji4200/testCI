97a0cb97f55edc9b53d34c69fb77070e1e2b9c74
qwqw4200 <qwqw4200@163.com>
2016-01-12 22:29:07 +0800

    test email when build success after failed .

--- TestCI/TestCI/ViewController.m
+++ TestCI/TestCI/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"%s" , __FUNCTION__)
+    NSLog(@"%s" , __FUNCTION__);
 }
 
 - (void)didReceiveMemoryWarning {
